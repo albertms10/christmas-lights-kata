e78aab1a093857ed95572080d033187374873dd6
Albert Mañosa <albertmasa2@gmail.com>
2022-11-08 16:36:32 +0100

    feat(main): follow Santa’s instructions

--- bin/main.dart
+++ bin/main.dart
@@ -1,9 +1,18 @@
+import 'dart:math';
+
 import 'package:christmas_lights_kata/grid.dart';
-import 'package:collection/collection.dart' show IterableExtension;
 
 void main(List<String> arguments) {
-  final size = int.tryParse(arguments.firstOrNull ?? '') ?? 50;
-  final grid = Grid(size);
+  final grid = Grid(1000)
+    ..turnOn(const Point(887, 9), const Point(959, 629))
+    ..turnOn(const Point(454, 398), const Point(844, 448))
+    ..turnOff(const Point(539, 243), const Point(559, 965))
+    ..turnOff(const Point(370, 819), const Point(676, 868))
+    ..turnOff(const Point(145, 40), const Point(370, 997))
+    ..turnOff(const Point(301, 3), const Point(808, 453))
+    ..turnOn(const Point(351, 678), const Point(951, 908))
+    ..toggle(const Point(720, 196), const Point(897, 994))
+    ..toggle(const Point(831, 394), const Point(904, 860));
 
-  print(grid);
+  print(grid.poweredLeds);
 }
